23f67737cea0773aff96a5c07db76974c4124958
RomanDoc <yadonistroman@mail.ru>
2024-07-03 19:58:18 +0700

    add data file

---
+++ data_preparetion.R
@@ -0,0 +1,9 @@
+# СберИндекс
+
+setwd('C:/Users/yadon/Skillbox/R/TrustUs_R/data/СберИндекс')
+
+cash_of <- read.csv('Доля безналичных платежей в торговом обороте.csv', sep = ';')
+index <- read.csv('Индекс потребительской активности.csv', sep = ';', fileEncoding='')
+turist <- read.csv('Количество внутренних туристов.csv', sep = ';')
+
+??encoding
